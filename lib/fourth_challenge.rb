34b4ae2a225450681498f9b7be0688d974828539
Jimmy Le <jimmyle414@gmail.com>
2020-04-01 00:25:47 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,7 +15,10 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [{
+        :first_friend => {name: "Steven", age: "30", attitude: "confused"},
+        :second_friend => {name: "Nurse", age: "44", attitude: "worried"}
+      }
         #your hashes here!
       ]
    }
