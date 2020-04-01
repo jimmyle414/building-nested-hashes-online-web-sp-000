0826a358d177c2c4ace0c139601ffe414f8710a1
Jimmy Le <jimmyle414@gmail.com>
2020-04-01 00:24:29 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [{
         :first_friend => {name: "Benvolio", age: "17", attitude: "worried"},
-        :second_friend => {name: "Mercutio", age: "18", 
+        :second_friend => {name: "Mercutio", age: "18", attitude: "hot-headed"}
       }
         #your hashes here!
       ]
